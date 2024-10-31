f3b42d575a89aa389613b4939f6fd632b0312438
fikrianggara <fikriseptriananggara@gmail.com>
2024-10-31 22:17:00 +0700

    init project, setup shadcn

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { ModeToggle } from "@/components/toggle-button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <ModeToggle />
+    </div>
+  );
+}
